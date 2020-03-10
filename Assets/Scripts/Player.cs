fcc0b1094e4b7fd0a53ea4a69f6b5b39d8f89a19
loChris <44450518+loChris@users.noreply.github.com>
2020-03-10 10:57:53 -0500

    added a player, player movement (still wip) and fixed some bugs in imported assets scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
