fceca5a69bc3e97db64769de5963b7e1ef97b333
loChris <44450518+loChris@users.noreply.github.com>
2020-03-10 11:06:25 -0500

    added gravity to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
+    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private float _gravity = 9.81f;
     
     // Start is called before the first frame update
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        GetGameComponents();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() => CalculateMovement();
+
+    void GetGameComponents()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _controller.Move(direction * Time.deltaTime);
+        _controller = GetComponent<CharacterController>();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+        
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
